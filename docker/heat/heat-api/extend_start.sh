a4757040d644bc657307f29738a80f7f3d26c8c7
fbermel <fabian.bermel@noris.de>
2020-03-05 11:22:19 +0100

    the script should check for heat_user_domain specifically and not grep for "heat" in a list of all domains.

--- docker/heat/heat-api/extend_start.sh
+++ docker/heat/heat-api/extend_start.sh
@@ -4,9 +4,8 @@
 # of the KOLLA_BOOTSTRAP variable being set, including empty.
 if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
     heat-manage db_sync
-    CURRENT_HEAT_DOMAIN_NAME=$(openstack domain list | grep heat | awk '{print $4}')
 
-    if [[ "heat_user_domain" != "$CURRENT_HEAT_DOMAIN_NAME" ]]; then
+    if ! openstack domain show heat_user_domain > /dev/null 2>&1; then
         openstack domain create heat_user_domain
         openstack user create --domain heat_user_domain heat_domain_admin --password ${HEAT_DOMAIN_ADMIN_PASSWORD}
         openstack role add --domain heat_user_domain --user-domain heat_user_domain --user heat_domain_admin admin
